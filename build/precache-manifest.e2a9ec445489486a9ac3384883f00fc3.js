10a25d6ffc810515475401675b29b4b40e6a8efa
Lobatskiy Andriy <lobatskiy31@gmail.com>
2020-06-22 10:32:32 +0300

    order

---
+++ build/precache-manifest.e2a9ec445489486a9ac3384883f00fc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db49d77998e638b27f2aa37e3042e665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddfe4a38c76bfd0c6b50",
+    "url": "/static/css/main.8ed87245.chunk.css"
+  },
+  {
+    "revision": "dcd54efbfab94fd132f0",
+    "url": "/static/js/2.bd766222.chunk.js"
+  },
+  {
+    "revision": "01f28827afd20553593a551e1ee6c97b",
+    "url": "/static/js/2.bd766222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfe4a38c76bfd0c6b50",
+    "url": "/static/js/main.673e1374.chunk.js"
+  },
+  {
+    "revision": "4a5e939be51c38258ecd",
+    "url": "/static/js/runtime-main.9f7cd10c.js"
+  },
+  {
+    "revision": "94f95d75542314d9597b52335689804f",
+    "url": "/static/media/bg_fonts_1.94f95d75.jpg"
+  }
+]);
